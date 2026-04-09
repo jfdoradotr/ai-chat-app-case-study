2818a7cba2e6acce2b1a66c6713b354cf54d7317
Juan Dorado <jfdoradotr@icloud.com>
2026-04-09 17:58:48 -0600

    Add a settings button

--- AIChatApp/AIChatApp/Core/Profile/ProfileView.swift
+++ AIChatApp/AIChatApp/Core/Profile/ProfileView.swift
@@ -5,9 +5,20 @@
 import SwiftUI
 
 struct ProfileView: View {
+  @State private var showSettingsView: Bool = false
+
   var body: some View {
     Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
       .navigationTitle("Profile")
+      .toolbar {
+        ToolbarItem(placement: .primaryAction) {
+          Button {
+            showSettingsView = true
+          } label: {
+            Label("Settings", systemImage: "gear")
+          }
+        }
+      }
   }
 }
 
