b25d26168721b324713fcba9bc3d74384432222d
Juan Dorado <jfdoradotr@icloud.com>
2026-04-09 18:35:26 -0600

    Inject the environemnt object and pass the finish button logic

--- AIChatApp/AIChatApp/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
+++ AIChatApp/AIChatApp/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
@@ -5,20 +5,26 @@
 import SwiftUI
 
 struct OnboardingCompletedView: View {
+  @Environment(AppState.self) private var appState
+
   var body: some View {
     VStack {
       Text("Onboarding Completed!")
         .frame(maxHeight: .infinity)
 
-      Button("Finish") {
-        // TODO: Finish onboarding and enter app
-      }
-      .buttonStyle(.primary)
+      Button("Finish", action: onFinishButtonPressed)
+        .buttonStyle(.primary)
     }
     .padding(16)
+  }
+
+  private func onFinishButtonPressed() {
+    // TODO: Other logic to complete onboarding
+    appState.updateViewState(showTabBarView: true)
   }
 }
 
 #Preview {
   OnboardingCompletedView()
+    .environment(AppState())
 }
