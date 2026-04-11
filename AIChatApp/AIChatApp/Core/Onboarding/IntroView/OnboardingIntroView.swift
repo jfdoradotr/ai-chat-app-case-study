6d83ba510c63c7095e18f9ed0405659cfb90a061
Juan Dorado <jfdoradotr@icloud.com>
2026-04-10 21:42:06 -0600

    Update the interpolation because iOS 26 does not accept + anymore

--- AIChatApp/AIChatApp/Core/Onboarding/IntroView/OnboardingIntroView.swift
+++ AIChatApp/AIChatApp/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -7,21 +7,7 @@ import SwiftUI
 struct OnboardingIntroView: View {
   var body: some View {
     VStack {
-      Group {
-        Text("Make your own ")
-        +
-        Text("avatars")
-          .foregroundStyle(.accent)
-          .fontWeight(.semibold)
-        +
-        Text(" and chat with them!\n\nHave ")
-        +
-        Text("real conversations")
-          .foregroundStyle(.accent)
-          .fontWeight(.semibold)
-        +
-        Text(" with AI generated responses.")
-      }
+      Text("Make your own \(Text("avatars").foregroundStyle(.accent).fontWeight(.semibold)) and chat with them!\n\nHave \(Text("real conversations").foregroundStyle(.accent).fontWeight(.semibold)) with AI generated responses.")
       .baselineOffset(6)
       .frame(maxHeight: .infinity)
       .padding(24)
