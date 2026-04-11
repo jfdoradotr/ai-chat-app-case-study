c21ef5e579659f8bf4286182cb2e8ae518023052
Juan Dorado <jfdoradotr@icloud.com>
2026-04-10 18:21:45 -0600

    Create the OnboardingIntroView only with the message

---
+++ AIChatApp/AIChatApp/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright © Juan Francisco Dorado Torres. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+  var body: some View {
+    VStack {
+      Text("Make your own ")
+      +
+      Text("avatars")
+        .foregroundStyle(.accent)
+        .fontWeight(.semibold)
+      +
+      Text(" and chat with them!\n\nHave ")
+      +
+      Text("real conversations")
+        .foregroundStyle(.accent)
+        .fontWeight(.semibold)
+      +
+      Text(" with AI generated responses.")
+    }
+  }
+}
+
+#Preview {
+  OnboardingIntroView()
+}
