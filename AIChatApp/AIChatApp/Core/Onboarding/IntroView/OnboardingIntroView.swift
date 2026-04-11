a078375edb66ec2999b2519e2672d183620184a1
Juan Dorado <jfdoradotr@icloud.com>
2026-04-10 18:24:42 -0600

    Add the button and improve padding

--- AIChatApp/AIChatApp/Core/Onboarding/IntroView/OnboardingIntroView.swift
+++ AIChatApp/AIChatApp/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -7,20 +7,34 @@ import SwiftUI
 struct OnboardingIntroView: View {
   var body: some View {
     VStack {
-      Text("Make your own ")
-      +
-      Text("avatars")
-        .foregroundStyle(.accent)
-        .fontWeight(.semibold)
-      +
-      Text(" and chat with them!\n\nHave ")
-      +
-      Text("real conversations")
-        .foregroundStyle(.accent)
-        .fontWeight(.semibold)
-      +
-      Text(" with AI generated responses.")
+      Group {
+        Text("Make your own ")
+        +
+        Text("avatars")
+          .foregroundStyle(.accent)
+          .fontWeight(.semibold)
+        +
+        Text(" and chat with them!\n\nHave ")
+        +
+        Text("real conversations")
+          .foregroundStyle(.accent)
+          .fontWeight(.semibold)
+        +
+        Text(" with AI generated responses.")
+      }
+      .baselineOffset(6)
+      .frame(maxHeight: .infinity)
+      .padding(24)
+
+      NavigationLink {
+        OnboardingCompletedView()
+      } label: {
+        Text("Continue")
+      }
+      .buttonStyle(.primary)
     }
+    .padding(24)
+    .font(.title3)
   }
 }
 
