3f02cd455192a79278fe53d1f4d45a850c74ff55
Juan Dorado <jfdoradotr@icloud.com>
2026-04-10 18:53:06 -0600

    Extract views into its own computed properties

--- AIChatApp/AIChatApp/Core/Onboarding/ColorView/OnboardingColorView.swift
+++ AIChatApp/AIChatApp/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -11,49 +11,57 @@ struct OnboardingColorView: View {
 
   var body: some View {
     ScrollView {
-      LazyVGrid(
-        columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
-        alignment: .center,
-        spacing: 16,
-        pinnedViews: [.sectionHeaders],
-        content: {
-          Section {
-            ForEach(profileColors, id: \.self) { color in
-              Circle()
-                .fill(.accent)
-                .overlay(
-                  color
-                    .clipShape(Circle())
-                    .padding(selectedColor == color ? 10 : 0)
-                )
-                .onTapGesture {
-                  selectedColor = color
-                }
-            }
-          } header: {
-            Text("Select a profile color")
-              .font(.headline)
-          }
-        }
-      )
-      .padding(.horizontal, 24)
+      colorGrid
+        .padding(.horizontal, 24)
     }
     .safeAreaInset(edge: .bottom, alignment: .center, spacing: 16) {
       ZStack {
-        if let selectedColor {
-          NavigationLink {
-            OnboardingCompletedView()
-          } label: {
-            Text("Continue")
-          }
-          .buttonStyle(.primary)
-          .transition(AnyTransition.move(edge: .bottom))
+        if selectedColor != nil {
+          ctaButton
+            .transition(AnyTransition.move(edge: .bottom))
         }
       }
       .padding(24)
       .background(Color(uiColor: .systemBackground))
     }
     .animation(.bouncy, value: selectedColor)
+  }
+
+  private var colorGrid: some View {
+    LazyVGrid(
+      columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+      alignment: .center,
+      spacing: 16,
+      pinnedViews: [.sectionHeaders],
+      content: {
+        Section {
+          ForEach(profileColors, id: \.self) { color in
+            Circle()
+              .fill(.accent)
+              .overlay(
+                color
+                  .clipShape(Circle())
+                  .padding(selectedColor == color ? 10 : 0)
+              )
+              .onTapGesture {
+                selectedColor = color
+              }
+          }
+        } header: {
+          Text("Select a profile color")
+            .font(.headline)
+        }
+      }
+    )
+  }
+
+  private var ctaButton: some View {
+    NavigationLink {
+      OnboardingCompletedView()
+    } label: {
+      Text("Continue")
+    }
+    .buttonStyle(.primary)
   }
 }
 
